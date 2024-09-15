80c9599c80d8fc7f13dcc7ff61d6aad202674a3e
Daniela Pinzon Guzman <danielapinzonguzman@gmail.com>
2024-09-15 12:18:22 +0100

    Close mobile nav when clicking internal page link

--- shared.js
+++ shared.js
@@ -1,5 +1,6 @@
 const siteNavHamburger = document.querySelector("#site-nav-hamburger");
 const mobileSiteNavClose = document.querySelector("#mobile-site-nav-close");
+const mobileSiteNavLinks = document.querySelectorAll(".mobile-site-nav-link");
 
 siteNavHamburger.addEventListener("click", () => {
   document.body.classList.add("show-mobile-nav");
@@ -8,3 +9,20 @@ siteNavHamburger.addEventListener("click", () => {
 mobileSiteNavClose.addEventListener("click", () => {
   document.body.classList.remove("show-mobile-nav");
 });
+
+for (const mobileSiteNavLink of mobileSiteNavLinks) {
+  const url = new URL(mobileSiteNavLink.href);
+
+  if (isInternalPageLink(url)) {
+    mobileSiteNavLink.addEventListener("click", (e) => {
+      document.body.classList.remove("show-mobile-nav");
+    });
+  }
+}
+
+function isInternalPageLink(url) {
+  return (
+    url.origin === window.location.origin &&
+    url.pathname === window.location.pathname
+  );
+}
