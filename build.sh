e3bf01a4bc85dc3545cf959aeb6c3f39aba050b2
Daniela Pinzon Guzman <danielapinzonguzman@gmail.com>
2025-02-04 15:45:11 +0000

    Optimise home page performance

---
+++ build.sh
@@ -0,0 +1,1 @@
+magick mogrify -format webp -quality 100 -path './images' './images/*.{png,jpg.jpeg}'
--- shared.js
+++ shared.js
@@ -1,22 +1,22 @@
-const siteNavHamburger = document.querySelector("#site-nav-hamburger");
-const mobileSiteNavClose = document.querySelector("#mobile-site-nav-close");
-const mobileSiteNavLinks = document.querySelectorAll(".mobile-site-nav-link");
+const siteNavHamburger = document.querySelector('#site-nav-hamburger')
+const mobileSiteNavClose = document.querySelector('#mobile-site-nav-close')
+const mobileSiteNavLinks = document.querySelectorAll('.mobile-site-nav-link')
 
-siteNavHamburger.addEventListener("click", () => {
-  document.body.classList.add("show-mobile-nav");
-});
+siteNavHamburger.addEventListener('click', () => {
+  document.body.classList.add('show-mobile-nav')
+})
 
-mobileSiteNavClose.addEventListener("click", () => {
-  document.body.classList.remove("show-mobile-nav");
-});
+mobileSiteNavClose.addEventListener('click', () => {
+  document.body.classList.remove('show-mobile-nav')
+})
 
 for (const mobileSiteNavLink of mobileSiteNavLinks) {
-  const url = new URL(mobileSiteNavLink.href);
+  const url = new URL(mobileSiteNavLink.href)
 
   if (isInternalPageLink(url)) {
-    mobileSiteNavLink.addEventListener("click", (e) => {
-      document.body.classList.remove("show-mobile-nav");
-    });
+    mobileSiteNavLink.addEventListener('click', (e) => {
+      document.body.classList.remove('show-mobile-nav')
+    })
   }
 }
 
@@ -24,5 +24,15 @@ function isInternalPageLink(url) {
   return (
     url.origin === window.location.origin &&
     url.pathname === window.location.pathname
-  );
+  )
 }
+
+window.addEventListener('load', () => {
+  const bgImgSections = document.querySelectorAll('[data-bg-img]')
+
+  setTimeout(() => {
+    for (const section of bgImgSections) {
+      section.style.backgroundImage = `url(${section.dataset.bgImg})`
+    }
+  }, 0)
+})
